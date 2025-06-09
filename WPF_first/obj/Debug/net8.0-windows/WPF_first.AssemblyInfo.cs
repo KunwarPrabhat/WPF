9e16a693779889d3929a844480528f0bf2f329c6
Raj Kunwar Prabhat <rajkunwarprabhat@gmail.com>
2025-06-09 19:46:29 +0530

    Some ui experiments

--- WPF_first/obj/Debug/net8.0-windows/WPF_first.AssemblyInfo.cs
+++ WPF_first/obj/Debug/net8.0-windows/WPF_first.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b73435a181ecaebc0d2760d111fdc8a3ee19a539")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
